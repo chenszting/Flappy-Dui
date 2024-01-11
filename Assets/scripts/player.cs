64137c3f603a1142f001df9169e9132664aaa1cf
chenszting <cheno3748@gmail.com>
2024-01-11 15:58:25 +0800

    地面碰撞

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -17,6 +17,13 @@ public class Player : MonoBehaviour
     {
         InvokeRepeating(nameof(Animatesprite), 0.15f, 0.15f);
     }
+    private void OnEnable()
+    {
+        Vector3 poisition = transform.position;
+        poisition.y = 0f;
+        transform.position = poisition;
+        direction = Vector3.zero;
+    }
     private void Update()
  {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
